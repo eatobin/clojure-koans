15822c6bdee61631f35e36c51450afddd2c1d613
Eric Tobin <eatobin@gmail.com>
2019-04-19 16:38:40 -0500

    progress

--- src/koans/03_lists.clj
+++ src/koans/03_lists.clj
@@ -18,13 +18,13 @@
   (= 0 (count '()))
 
   "The rest, when nothing is left, is empty"
-  (= __ (rest '(100)))
+  (= '() (rest '(100)))
 
   "Construction by adding an element to the front is easy"
-  (= __ (cons :a '(:b :c :d :e)))
+  (= '(:a :b :c :d :e) (cons :a '(:b :c :d :e)))
 
   "Conjoining an element to a list isn't hard either"
-  (= __ (conj '(:a :b :c :d) :e))
+  (= '(:e :a :b :c :d) (conj '(:a :b :c :d) :e))
 
   "You can use a list like a stack to get the first element"
   (= __ (peek '(:a :b :c :d :e)))
