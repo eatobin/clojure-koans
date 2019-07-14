f41422cdf9bc52e2235ce8fc2d7261d77a8c9fbb
Eric Tobin <eatobin@gmail.com>
2019-07-14 18:36:04 -0500

    progress

--- src/koans/18_quote.clj
+++ src/koans/18_quote.clj
@@ -10,7 +10,7 @@
   (= (quote (1 2 3 4 5)) '(1 2 3 4 5))
 
   "You can quote symbols as well as lists... without evaluation!"
-  (= __ (let [age 9] (quote age)))
+  (= age (let [age 9] (quote age)))
 
   "You can use a literal list as a data collection without having Clojure try to call a function"
   (= (cons 1 (__ (2 3))) (list 1 2 3) (cons 1 [2 3]))
