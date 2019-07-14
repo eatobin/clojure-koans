4fb38f7dbd5ba0f466cbc0095a7ea9791f44d39d
Eric Tobin <eatobin@gmail.com>
2019-07-14 18:47:13 -0500

    18 done

--- src/koans/18_quote.clj
+++ src/koans/18_quote.clj
@@ -16,10 +16,10 @@
   (= (cons 1 (quote (2 3))) (list 1 2 3) (cons 1 [2 3]))
 
   "The quote affects all of its arguments, not just the top level"
-  (= (list 1 __) '(1 (+ 2 3)))
+  (= (list 1 '(+ 2 3)) '(1 (+ 2 3)))
 
   "Syntax-quote (`) acts similarly to the normal quote"
-  (= (list __ __ __) `(1 2 3) '(1 2 3))
+  (= (list 1 2 3) `(1 2 3) '(1 2 3))
 
   "Unquote (~) within a syntax-quoted expression lets you mark specific expressions as requiring evaluation"
-  (= (list __ __) `(1 ~(+ 2 3)) '(1 5)))
+  (= (list 1 5) `(1 ~(+ 2 3)) '(1 5)))
